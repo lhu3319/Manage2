df8a311307a08b7ba27e547081bfb6b1d287bba1
Han <lhu3319@nate.com>
2019-01-22 11:56:00 +0900

    4214124214214

--- Solution1/WebApplication1/Program.cs
+++ Solution1/WebApplication1/Program.cs
@@ -18,7 +18,7 @@ namespace WebApplication1
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+           WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>().UseUrls("http://*:80");
     }
 }
